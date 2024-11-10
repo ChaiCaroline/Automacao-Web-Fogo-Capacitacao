e633257d32e5aeb7ee5cff60fef1d4be615cc67d
chaiene <chaiene_caroline@hotmail.com>
2024-11-09 21:17:04 -0300

    :sparkles: feat: Adicionando os elementos do carrinho

---
+++ src/test/java/com/trilha/automacao/pages/CartPage.java
@@ -0,0 +1,10 @@
+package com.trilha.automacao.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPage {
+
+    @FindBy(className = "btn btn-success")
+    public static WebElement btnBuy;
+}
