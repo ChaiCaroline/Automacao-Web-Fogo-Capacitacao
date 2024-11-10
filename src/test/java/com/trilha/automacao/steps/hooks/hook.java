3f49c242a621dbe153e2ba48d70a147f4bb8de32
chaiene <chaiene_caroline@hotmail.com>
2024-11-09 23:30:00 -0300

    :sparkles: feat: adicionando ação para fechar o webdriver ao fim do ultimo cenário

--- src/test/java/com/trilha/automacao/steps/hooks/hook.java
+++ src/test/java/com/trilha/automacao/steps/hooks/hook.java
@@ -1,23 +1,13 @@
 package com.trilha.automacao.steps.hooks;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.trilha.automacao.utils.Configuration.quitDriver;
 
 public class hook {
 
-    public static WebDriver driver;
-
-    @Before
-    public void createWebDriver() {
-            driver = new ChromeDriver();
-    }
-
-
-
     @After
-    public void finalizar(){
-    driver.quit();
+    public void finish(){
+        quitDriver();
     }
 }
