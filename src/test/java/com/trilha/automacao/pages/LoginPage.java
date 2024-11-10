3afeae2c109c1fee7b55e4e062c564b8abe4a5ae
chaiene <chaiene_caroline@hotmail.com>
2024-11-09 21:17:52 -0300

    :truck: chore: alterando o nome de HomePage para LoginPage

--- src/test/java/com/trilha/automacao/pages/LoginPage.java
+++ src/test/java/com/trilha/automacao/pages/LoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class HomePage {
+public class LoginPage {
 
     @FindBy(id = "sign-username")
     public static WebDriver inputLogin;
