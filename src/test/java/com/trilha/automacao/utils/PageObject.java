2d8501d888acafc1fad6b5b6f21e2a06b462bde6
ChaiCaroline <chaiene_caroline@hotmail.com>
2024-11-10 20:22:12 -0300

    :sparkles: feat: Criação da classe de esperas e page object

---
+++ src/test/java/com/trilha/automacao/utils/PageObject.java
@@ -0,0 +1,19 @@
+package com.trilha.automacao.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PageObject {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public String urlBugBank = "https://bugbank.netlify.app/";
+
+    public PageObject(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+}
