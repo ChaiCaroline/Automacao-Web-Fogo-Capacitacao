5ca68b1b2913391add0cb98c1804e71707c63952
chaiene <chaiene_caroline@hotmail.com>
2024-11-09 23:30:37 -0300

    :sparkles: feat: começando a implementação das etapas de execução do bdd de registro.

--- src/test/java/com/trilha/automacao/steps/RegisterStep.java
+++ src/test/java/com/trilha/automacao/steps/RegisterStep.java
@@ -1,18 +1,27 @@
 package com.trilha.automacao.steps;
 
+import com.trilha.automacao.pages.NavPage;
+import com.trilha.automacao.utils.Configuration;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class RegisterStep {
 
+    NavPage navPage;
+
+    public RegisterStep() {
+        navPage = new NavPage();
+    }
+
     @Dado("que estou na página inicial")
     public void que_estou_na_pagina_inicial() {
-
+        Configuration.getDriver();
+        Configuration.acessURL("https://www.demoblaze.com/index.html");
     }
     @Quando("eu clicar em sign up")
     public void eu_clicar_em_sign_up() {
-
+        NavPage.btnSignUp.click();
     }
     @Quando("inserir meu nome, {string} e {string} para cadastro")
     public void inserir_meu_nome_e_para_cadastro(String string, String string2) {
